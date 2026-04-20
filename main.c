fbca45c1678a06f041192f70818a8436bd07fc8f
Ashok S.R <ashok.106sr@gmail.com>
2026-04-20 22:14:39 +0530

    add some stuff

--- main.c
+++ main.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unisrd.h>
+#include <signal.h>
+#include <time.h>
+
 
 int main() {
-    printf("Hello");
 
 
     return 0;
