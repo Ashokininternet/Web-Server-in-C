517c2e42b217d11e3b161f44d560727b7a4c1afe
Ashok S.R <ashok.106sr@gmail.com>
2026-04-25 17:43:43 +0530

    apr 25 commit

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include <asm-generic/socket.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,9 +9,34 @@
 #include <signal.h>
 #include <time.h>
 
+#define PORT 2728
+#define BACKLOG 10
+#define SIZE 1024
 
 int main() {
-    printf("Hello \n");
+  int serverSocket;
 
-    return 0;
+
+  struct sockaddr_in serverAddress;
+  serverAddress.sin_family = AF_INET;
+  serverAddress.sin_port = htons(PORT);
+  serverAddress.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+
+  serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+  setsockopt(serverSocket, SOL_SOCKET, SO_REUSEADDR, &(int){1}, sizeof(int));
+
+  if (bind(serverSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) < 0)
+  {
+    printf("Error: The server is not bound to the address.\n");
+    return 1;
+  }
+  if (listen(serverSocket, BACKLOG) < 0)
+  {
+    printf("Error: The server is not listening.\n");
+    return 1;
+  }
+  
+  
+
+  return 0;
 }
