fed6a61b4baa0b63f302166b44ca3ede41cea481
Ashok S.R <ashok.106sr@gmail.com>
2026-04-25 19:59:59 +0530

    Add serving Request

--- main.c
+++ main.c
@@ -15,7 +15,8 @@
 
 int main() {
   int serverSocket;
-
+  int clientSocket;
+  char *request;
 
   struct sockaddr_in serverAddress;
   serverAddress.sin_family = AF_INET;
@@ -35,8 +36,26 @@ int main() {
     printf("Error: The server is not listening.\n");
     return 1;
   }
+
+  char hostBuffer[NI_MAXHOST], serviceBuffer[NI_MAXSERV];
+  int error = getnameinfo((struct sockaddr *)&serverAddress, sizeof(serverAddress), hostBuffer, sizeof(hostBuffer), serviceBuffer, sizeof(serviceBuffer), 0);
+
+  if(error != 0){
+    printf("Error: %s\n", gai_strerror(error));
+  }
+
+  printf("\n Server is listening on http://%s:%s/\n\n", hostBuffer, serviceBuffer);
   
-  
+  request = (char *)malloc(SIZE * sizeof(char));
+  char method[10], rout[100];
+
+  clientSocket = accept(serverSocket, NULL, NULL);
+  read(clientSocket, request, SIZE);
+
+  sscanf(request,"%s %s",method, rout );
+  printf("%s %s",method, rout );
+  free(request);
+
 
   return 0;
 }
